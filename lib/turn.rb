7b2a0beba4e0b26dd89343ec865526028835b1d2
Clark Chen <yyunqingchenn@gmail.com>
2019-08-30 07:45:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -12,14 +12,14 @@ def input_to_index(user_input)
   return index 
 end 
     
-def valid_move?(board,index)
-  def position_taken?(board, index)
+def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false 
   else
     return true 
   end 
-end
+
+
 
 def on_board?(num)
   if num.between?(0,8) == true
